1e1432d4428dd132631096393605c1018ce82937
niekcandaele <niek@candaele.dev>
2021-06-27 20:40:24 +0200

    refactor: split run into runAll

--- src/main.test.ts
+++ src/main.test.ts
@@ -2,31 +2,24 @@ import type { App, TFile } from "obsidian";
 import Sinon from "sinon";
 import { expect } from "./expect.test";
 import LocalImagesPlugin from "./main";
-import { run } from "./run";
+import { run, runAll } from "./run";
 
-const manifest = require("../manifest.json");
-
-// Credits: Wikipedia :)
-const mockNote = {
-  input: `
-Obsidian
-
-[![Lipari-Obsidienne (5).jpg](https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Lipari-Obsidienne_%285%29.jpg/220px-Lipari-Obsidienne_%285%29.jpg)](https://en.wikipedia.org/wiki/File:Lipari-Obsidienne_(5).jpg)
-
-General
-
-Category
-
-[Volcanic glass](https://en.wikipedia.org/wiki/Volcanic_glass "Volcanic glass")
-`,
-  output:
-    '\nObsidian\n\n[![Original source: https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Lipari-Obsidienne_%285%29.jpg/220px-Lipari-Obsidienne_%285%29.jpg](/)](https://en.wikipedia.org/wiki/File:Lipari-Obsidienne_(5).jpg)\n\nGeneral\n\nCategory\n\n[Volcanic glass](https://en.wikipedia.org/wiki/Volcanic_glass "Volcanic glass")\n',
+const mockNotes = {
+  mockNoteSimple: {
+    input: `![Lipari-Obsidienne (5).jpg](https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Lipari-Obsidienne_%285%29.jpg/220px-Lipari-Obsidienne_%285%29.jpg)`,
+    output: `![Original source: https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Lipari-Obsidienne_%285%29.jpg/220px-Lipari-Obsidienne_%285%29.jpg](/img/220pxLipariObsidienne28529.jpg)`,
+  },
+  // Credits: Wikipedia :)
+  mockNoteNormalLink: {
+    input: `[Volcanic glass](https://en.wikipedia.org/wiki/Volcanic_glass "Volcanic glass")`,
+    output: `[Volcanic glass](https://en.wikipedia.org/wiki/Volcanic_glass "Volcanic glass")`,
+  },
 };
 
 const getMockApp = () => ({
   vault: {
-    getFiles: Sinon.stub().returns([{}]),
-    read: Sinon.stub().resolves(mockNote.input),
+    getMarkdownFiles: Sinon.stub().returns(Object.values(mockNotes)),
+    read: Sinon.stub().callsFake((mockNote) => mockNote.input),
     createFolder: Sinon.stub(),
     createBinary: Sinon.stub().resolves({ path: "/" }),
     modify: Sinon.stub(),
@@ -45,16 +38,26 @@ describe("main", () => {
   });
 
   it("Full flow", async () => {
-    await run(mockApp as unknown as App);
-    expect(mockApp.vault.modify).to.have.been.calledOnceWith(
-      Sinon.match.any,
-      mockNote.output
-    );
+    await runAll(mockApp as unknown as App);
+
+    for (const noteName in mockNotes) {
+      if (Object.prototype.hasOwnProperty.call(mockNotes, noteName)) {
+        //@ts-ignore
+        const note = mockNotes[noteName];
+        expect(mockApp.vault.modify).to.have.been.calledWith(
+          Sinon.match.any,
+          note.output
+        );
+      }
+    }
   });
 
   it("Can handle root storage", async () => {
     mockApp.fileManager.getNewFileParent.returns({ path: "/" });
-    await run(mockApp as unknown as App);
+    await run(
+      mockApp as unknown as App,
+      mockNotes.mockNoteSimple as unknown as TFile
+    );
 
     expect(mockApp.vault.createBinary).to.have.been.calledOnceWith(
       "/img/220pxLipariObsidienne28529.jpg",
@@ -64,7 +67,10 @@ describe("main", () => {
 
   it("Can handle sub dir storage", async () => {
     mockApp.fileManager.getNewFileParent.returns({ path: "/test" });
-    await run(mockApp as unknown as App);
+    await run(
+      mockApp as unknown as App,
+      mockNotes.mockNoteSimple as unknown as TFile
+    );
 
     expect(mockApp.vault.createBinary).to.have.been.calledOnceWith(
       "/test/img/220pxLipariObsidienne28529.jpg",
