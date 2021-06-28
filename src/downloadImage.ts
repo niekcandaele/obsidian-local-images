6342fb7ccb888c962bc284415477b8ce8d130b40
niekcandaele <niek@candaele.dev>
2021-06-28 14:04:45 +0200

    fix: CORS issue with node-fetch

--- src/downloadImage.ts
+++ src/downloadImage.ts
@@ -1,7 +1,14 @@
-import fetch from "node-fetch";
+import got from "got";
+
+/**
+ * Downloads the image
+ * We use got here, a library that only supports node
+ * This is to get around a limitation of Electron blocking CORS requests with node-fetch
+ * @param url The HTTP URL to the image
+ * @returns
+ */
 
 export async function downloadImage(url: string): Promise<ArrayBuffer> {
-  const imageRes = await fetch(url);
-  const imageData = await imageRes.arrayBuffer();
-  return imageData;
+  const res = await got(url, { responseType: "buffer" });
+  return res.body;
 }
